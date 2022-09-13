98fc151506c04bd3c0a1183d7dceb7a1bc4f8629
maxxxutko99 <bafldalovdursun78@gmail.com>
2022-09-13 03:12:45 +0300

    hover was created

--- src/js/components/otherPages.js
+++ src/js/components/otherPages.js
@@ -1,7 +1,28 @@
 export function otherPages() {
   const fullPage = document.querySelector('.full-page');
 
+  const sectionWorks = document.querySelector('.works');
+  const sectionAbout = document.querySelector('.about');
+
   if(!fullPage) {
     document.documentElement.style.overflow = 'visible';
+
+    if(sectionWorks) {
+      setColor(sectionWorks);
+    } else if(sectionAbout) {
+      setColor(sectionAbout);
+    }
+  }
+
+  function setColor(el) {
+    if(el) {
+      window.addEventListener('scroll', () => {
+        if(window.scrollY >= el.offsetHeight) {
+          document.documentElement.classList.add('header-color');
+        } else {
+          document.documentElement.classList.remove('header-color');
+        }
+      });
+    }
   }
 }
