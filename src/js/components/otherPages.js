fe9d47c01d7c266ddacb0b810a7de24913f2a2bb
maxxxutko99 <bafldalovdursun78@gmail.com>
2022-09-15 02:36:18 +0300

    adaptation for 1399 breakpoint

--- src/js/components/otherPages.js
+++ src/js/components/otherPages.js
@@ -11,6 +11,14 @@ export function otherPages() {
       setColor(sectionWorks);
     } else if(sectionAbout) {
       setColor(sectionAbout);
+
+      const tooltipTxt = document.querySelector('.tooltip__txt');
+      const tooltipBtn = document.querySelector('.tooltip__btn');
+      if(vars.htmlEl.classList.contains('page--android') || vars.htmlEl.classList.contains('page--ios')) {
+        tooltipBtn.addEventListener('click', (e) => {
+          tooltipTxt.classList.toggle('tooltip__txt--mobile-active');
+        });
+      }
     }
   }
 
